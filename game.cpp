fa5754cbfe935675617d836a5c8e2121fe3c94e2
mindoll1101 <mindoll1101@naver.com>
2024-03-15 21:24:50 +0900

    fix: 랜덤 숫자가 겹치지 않고 정답이 미리 출력되지 않도록 변경

--- game.cpp
+++ game.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include<array>
 
-void print_status(int strike, int ball);
+void print_status(int strike, int ball, int chances);
 int random();
 
-void strike_ball(int a, int b){
+void strike_ball(int a, int b, int chances){
     int strike = 0, ball = 0;       //variable to save strikes and balls.
 
     std::array<int, 3> num_a;       //array to save answer and guess.
@@ -28,18 +28,20 @@ void strike_ball(int a, int b){
             }
         }
     }
-    print_status(strike, ball);
+    print_status(strike, ball, chances);
 
 }
 
 void game(){
     int a = random(); //create random answer
-    std::cout << "Answer is " << a/100 << (a%100)/10 << a%10 << std::endl;
     int b = 0;
-    while(a != b){
+    int chances = 5;
+    while(a != b && chances > 0){
+        std::cout << chances << " chances left." << std::endl;
         std::cout << "Enter a guess: ";
         std::cin >> b;
-        strike_ball(a, b);
+        strike_ball(a, b, chances);
+        chances -= 1;
     }
 }
 
--- util.cpp
+++ util.cpp
@@ -2,9 +2,12 @@
 #include<ctime>
 #include<random>
 
-void print_status(int strike, int ball){
+void print_status(int strike, int ball, int chances){
     if(strike == 3){
         std::cout << "You win!" << std::endl;
+    }
+    else if(chances < 1){
+        std::cout << "You lose!" << std::endl;
     }
     else{
         std::cout << "Strikes: " << strike << ", Balls: " << ball << std::endl;
@@ -13,7 +16,17 @@ void print_status(int strike, int ball){
 
 int random(){
     std::mt19937 gen(time(NULL));
-    std::uniform_int_distribution<int> dist(0, 999);
+    std::uniform_int_distribution<int> dist(0, 9);
     int a = dist(gen); //create random answer
-    return a;
+    int b = dist(gen);
+    int c = dist(gen);
+    while(a == b || b == c || a == c){
+        if(a == b){
+            b = dist(gen);
+        }
+        if(b == c){
+            c = dist(gen);
+        }
+    }
+    return a * 100 + b * 10 + c;
 }
